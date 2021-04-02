683438b4ae5797c9e3644fd5b96977bdfe9f6326
Mike Perry <mikeperry-git@torproject.org>
2021-04-02 02:39:02 +0000

    Add simulator test file and header.

---
+++ src/test/circuitpadding_sim_arg.h
@@ -0,0 +1,7 @@
+#ifndef TOR_CIRCUITPADDING_SIM_ARG_H
+#define TOR_CIRCUITPADDING_SIM_ARG_H
+
+extern const char *circpad_sim_arg_client_trace, *circpad_sim_arg_relay_trace;
+extern uint32_t circpad_sim_client_circid;
+
+#endif /* !defined(TOR_CIRCUITPADDING_SIM_ARS_) */
--- src/test/testing_common.c
+++ src/test/testing_common.c
@@ -313,6 +313,17 @@ main(int c, const char **v)
       if (den == 0 || num == 0 || num > den) {
         printf("--fraction expects a valid fraction as an input.\n");
       }
+    } else if (!strcmp(v[i], "--circpadsim")) {
+      if (i + 4 > c) {
+        printf("not enough arguments for --circpadsim. "
+               "expect %s %s, exiting.\n", v[0],
+               " --circpadsim <client-trace> <relay-trace> <client_cirdid>");
+        return 1;
+      }
+      circpad_sim_arg_client_trace = v[i+1];
+      circpad_sim_arg_relay_trace = v[i+2];
+      circpad_sim_client_circid = (uint32_t)strtol(v[i+3], NULL, 10);
+      i += 2;
     } else {
       v[i_out++] = v[i];
     }
